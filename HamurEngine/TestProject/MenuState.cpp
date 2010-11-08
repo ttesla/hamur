979f2abed6d6c500d5561ffb3b456f85c402e0cb
edufg88@gmail.com <edufg88@gmail.com@5bc8c2ac-02c6-11de-bd8f-71c2068a397f>
2010-11-08 22:28:45 +0000

    - Now a test main menu is showed

--- HamurEngine/TestProject/MenuState.cpp
+++ HamurEngine/TestProject/MenuState.cpp
@@ -14,12 +14,37 @@ MenuState::~MenuState()
 
 void MenuState::Enter()
 {
-	
+	int w = HamurOpenGL::GetInstance()->GetScreenWidth();
+	int h = HamurOpenGL::GetInstance()->GetScreenHeight();
+
+	HamurVec3 c;
+
+	// Background
+	c.x = w/2; c.y = h/2;
+	HAMURENGINE->SetBackgroundColor(HamurColor::WHITE);
+	//background = new Panel ("backgroundSelection", c, "Graphics/testbg2.png", HamurOpenGL::GetInstance()->GetScreenWidth(), HamurOpenGL::GetInstance()->GetScreenHeight());
+	c.y = 75;
+	// Title
+	title = new Panel("titleMenuPanel", c, "Graphics/menutitle.png", 700, 105);
+	// Some buttons
+	c.y = 250;
+	playButton = new Button("playMenuButton", c, "Graphics/menuplay.png", 200, 50);
+	c.y += 50;
+	howToPlayButton = new Button("howToPlayMenuButton", c, "Graphics/menuhowtoplay.png", 200, 50);
+	c.y += 50;
+	exitButton = new Button("exitMenuButton", c, "Graphics/menuexit.png", 200, 50);
 }
 
 void MenuState::Update(float deltaTime)
 {
-
+	if (this->playButton->isPushed())
+	{
+		HAMURSTATEMR->ChangeState("SelectionState");
+	}
+	if (this->exitButton->isPushed())
+	{
+		HAMURENGINE->Stop();	
+	}
 }
 
 void MenuState::Draw(float deltaTime)
@@ -29,5 +54,8 @@ void MenuState::Draw(float deltaTime)
 
 void MenuState::Exit()
 {
-
+	HAMURWORLD->DeleteObject("titleMenuPanel");
+	HAMURWORLD->DeleteObject("playMenuButton");
+	HAMURWORLD->DeleteObject("howToPlayMenuButton");
+	HAMURWORLD->DeleteObject("exitMenuButton");
 }
